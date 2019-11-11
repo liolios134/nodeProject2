5ba20be18266383adb37027b67af28ad3ceaac6d
JLio <liolios134@gmail.com>
2019-11-11 20:03:28 +0200

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 const chalk= require('chalk');
+const products= require('./products.json');
 
-console.log(chalk.bgBlue('Hello World'));
+console.log(products.length);
+
+products.forEach(prods=>{
+    console.log(prods.name + " : " + prods.price + "\u20ac");
+});
+
+let sum = 0;
+let max = products[0].price;
+let min = products[0].price;
+
+for(let p of products) {
+    sum = sum + p.price;
+    if (p.sale) {
+        console.log(p.name + " : " + chalk.bgBlue(p.sale)  + "\u20ac");
+    }else{
+        console.log(p.name + " : " + p.price + "\u20ac");
+    }
+    if(max<p.price) {
+            max = p.sale;
+    }
+    else{
+        max=p.price;
+    }
+    if(min>p.price){
+        min= p.price;
+    }
+}
+
+console.log(max);
+console.log(min);
+console.log(sum);
+console.log("average:" + sum/products.length);
